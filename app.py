721c92fb4236261d519e10462a004358e38f7c00
Endika Aguirre <endikaguirregu@gmail.com>
2025-12-02 18:14:31 +0100

    add share to treu

--- app.py
+++ app.py
@@ -111,4 +111,4 @@ with gr.Blocks(title="Image Classification - MLOps Lab 2", theme=gr.themes.Soft(
 
 # Launch the app
 if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=7860)
+    demo.launch(server_name="0.0.0.0",  share=True, server_port=7860)
